648eda9f24c17d0b0bc79c29ef9719ff74d5f63b
jacoc <jaco.cloete@student.hu.nl>
2017-09-08 13:14:34 +0200

    dit is een versie

---
+++ les 03/exercise aangeboden in les 2.py
@@ -0,0 +1,7 @@
+naam =  input('Wat is je naam:')
+leeftijd = eval(input('Wat is je leeftijd:'))
+
+if (leeftijd >=  18):
+    print(naam + ', Je mag stemmen')
+else:
+    print(naam + ', Je mag niet stemmen')
